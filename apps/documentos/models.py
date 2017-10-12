aa3c9e67c165bc5c87f4bf6ddc3623c511a2e017
Jair Buitron <jairnet5@gmail.com>
2017-10-12 06:13:46 -0500

    model to migrate

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Archivo(models.Model):
+
+	TIPO_ARCHIVO = (
+		('GDG','Gestion Directiva'),
+		('SIC','Sistema Integrado de Calidad'),
+		('COM','Compras'),
+		('LOG','Logistica'),
+		('COM','Comercial'),
+		('SER','Servicio al Cliente'),
+		('APO','Apoyo'),
+		)
+	tipo = models.CharField(max_length=20, choices=TIPO_ARCHIVO)
+	nombre = models.CharField(max_length=50)
+	fecha = models.DateField()
+	descripcion = models.TextField(max_length=100)
+	archive = models.FileField(upload_to='docuemntos/')
+
+	class Meta:
+		ordering = ["nombre"]
+		verbose_name_plural = "Archivos"
+
+	def __str__(self): # __unicode__ en Python 2
+		return self.nombre
+	
