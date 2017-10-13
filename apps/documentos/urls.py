df895c2616e15c9664d388622febfeadc1966d00
Jair Buitron <jairnet5@gmail.com>
2017-10-13 01:33:18 -0500

    index a construir

---
+++ apps/documentos/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from apps.documentos.views import Index
+urlpatterns = [
+    url(r'^$', Index.as_view(),name='index'),
+]
--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 
+from .models import Archivo
 # Create your views here.
+
+class Index(ListView):
+	model = Archivo
+	template_name = "documentos/index.html"
